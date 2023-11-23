ab35226082709d2b17bdad72f6ff20371f1cc9cb
chaiyakornkhunthong <bas31234@gmail.com>
2023-11-23 23:33:29 +0700

    add check user on  main page

--- app/src/main/java/com/chaiyakornkhunthong/pilotassistant_android/MainActivity.java
+++ app/src/main/java/com/chaiyakornkhunthong/pilotassistant_android/MainActivity.java
@@ -5,10 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+valu data =new valu();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (data.getAuth().getCurrentUser()== null){
+
+        }
     }
 }
--- app/src/main/java/com/chaiyakornkhunthong/pilotassistant_android/valu.java
+++ app/src/main/java/com/chaiyakornkhunthong/pilotassistant_android/valu.java
@@ -1,4 +1,15 @@
 package com.chaiyakornkhunthong.pilotassistant_android;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class valu {
+    private FirebaseAuth Auth = FirebaseAuth.getInstance();
+
+    public FirebaseAuth getAuth() {
+        return Auth;
+    }
+
+    public void setAuth(FirebaseAuth auth) {
+        Auth = auth;
+    }
 }
